ad43f2f81f9dca0f37b64a772f11c3f87b2f50dd
Sairah234 <89387785+Sairah234@users.noreply.github.com>
2021-08-25 15:49:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
+difference= 0;
+rightWristX= 0;
+leftWristX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
 
     canvas = createCanvas(550, 500);
-    canvas.position(600,150);
+    canvas.position(560,150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+    poseNet.on('pose', gotPoses);  
 }
 
 function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
 
-function draw() {
-    background('#90ee90');
+function draw() 
+{
+    background('#ADD8E6');
+
+    document.getElementById("font_size").innerHTML = "The font size of text will be = " + difference +"px";
+    textSize(difference);
+    fill('#F90093');
+    text('Sairah',50,400);
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX + " rightWristX = "+rightWristX + "difference =" + difference);
     }
 }
